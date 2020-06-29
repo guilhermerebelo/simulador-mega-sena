4f2953b4c0c108f0764687c83bee3df805b724f9
Guilherme dos Santos Rebelo <guilherme.rebelo@betha.com.br>
2020-06-29 02:28:17 -0300

    criando layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,78 @@
-import React from 'react';
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  return (
-    <div className="App"> hello word
-    </div>
-  );
+    return (
+        <>
+            <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    <FaGithub />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+
+            <div style={{ height: 120 }}></div>
+
+            <div className="container">
+                <section>
+                    <h4 className="font-weight-light">
+                        JOGUE 6 NÚMEROS E TENTE A SORTE!
+                    </h4>
+                    <br />
+                    <div className="input-group input-group-lg">
+                        <input
+                            type="text"
+                            className="col-md-2 form-control"
+                            aria-label="Exemplo do tamanho do input"
+                            aria-describedby="inputGroup-sizing-lg"
+                        />
+                        <input
+                            type="text"
+                            className="col-md-2 form-control"
+                            aria-label="Exemplo do tamanho do input"
+                            aria-describedby="inputGroup-sizing-lg"
+                        />
+                        <input
+                            type="text"
+                            className="col-md-2 form-control"
+                            aria-label="Exemplo do tamanho do input"
+                            aria-describedby="inputGroup-sizing-lg"
+                        />
+                        <input
+                            type="text"
+                            className="col-md-2 form-control"
+                            aria-label="Exemplo do tamanho do input"
+                            aria-describedby="inputGroup-sizing-lg"
+                        />
+                        <input
+                            type="text"
+                            className="col-md-2 form-control"
+                            aria-label="Exemplo do tamanho do input"
+                            aria-describedby="inputGroup-sizing-lg"
+                        />
+                        <input
+                            type="text"
+                            className="col-md-2 form-control"
+                            aria-label="Exemplo do tamanho do input"
+                            aria-describedby="inputGroup-sizing-lg"
+                        />
+                    </div>
+                    <br/>
+                    <div className="mx-auto" style={{'width': 120}}>
+                        <button className="btn btn-success btn-block">JOGAR</button>
+                    </div>
+                </section>
+            </div>
+        </>
+    );
 }
 
 export default App;
